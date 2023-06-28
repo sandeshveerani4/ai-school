0b0bd663a7f5609ccb8192a3d68ac52320f4c3c6
sandeshveerani4 <sandeshveerani4@gmail.com>
2023-06-28 20:37:35 +0200

    fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/login",
     signOut: "/dashboard/logout",
