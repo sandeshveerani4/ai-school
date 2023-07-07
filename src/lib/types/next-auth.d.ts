a853138a440354a8983bf5f890be05897aacfad2
sandeshveerani4 <sandeshveerani4@gmail.com>
2023-07-07 15:46:07 +0200

    fixed type issues

--- src/lib/types/next-auth.d.ts
+++ src/lib/types/next-auth.d.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import { User } from "../authorize";
-
+interface IUser extends User {}
 declare module "next-auth" {
+  interface User extends IUser {
+    accessToken: string;
+  }
+
   interface Session {
     user: User;
   }
